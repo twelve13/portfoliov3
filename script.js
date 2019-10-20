677437f48f48be40a298e7df8e9e980142f06d24
Caroline Chen <carolinecaroline@gmail.com>
2019-10-20 16:22:07 -0400

    style the projects section

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
 let sampleTrackerProject = document.getElementById("sample-tracker-project");
-let sampleTrackerBlurb = document.getElementById("sample-tracker-blurb");
+let sampleTrackerTitle = document.getElementById("sample-tracker-title");
+let cacheProject = document.getElementById("cache-project");
+let cacheTitle = document.getElementById("cache-title");
+let knittingProject = document.getElementById("knitting-project");
+let knittingTitle = document.getElementById("knitting-title");
 
-sampleTrackerProject.addEventListener("mouseover", function() {
-	sampleTrackerBlurb.classList.add("active")
+function moveTitleLeft (project) {
+	var pos = 0;
+	var interval = setInterval(frame, 5);
+	function frame() {
+		if (pos == 50) {
+			clearInterval(interval);
+		} else {
+			pos++;
+			project.style.right = pos + "%";
+		}
+	}
+}
+
+function moveTitleRight (project) {
+	var pos = 50;
+	var interval = setInterval(frame, 5);
+	function frame() {
+		if (pos == 0) {
+			clearInterval(interval);
+		} else {
+			pos--;
+			project.style.right = pos + "%";
+		}
+	}
+}
+
+sampleTrackerProject.addEventListener("mouseenter", function() {
+	moveTitleLeft(sampleTrackerTitle);
 })
 
 sampleTrackerProject.addEventListener("mouseleave", function() {
-	sampleTrackerBlurb.classList.remove("active")
+	moveTitleRight(sampleTrackerTitle);
+})
+
+cacheProject.addEventListener("mouseenter", function() {
+	moveTitleLeft(cacheTitle);
+})
+
+cacheProject.addEventListener("mouseleave", function() {
+	moveTitleRight(cacheTitle);
+})
+
+knittingProject.addEventListener("mouseenter", function() {
+	moveTitleLeft(knittingTitle);
+})
+
+knittingProject.addEventListener("mouseleave", function() {
+	moveTitleRight(knittingTitle);
 })
